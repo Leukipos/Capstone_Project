10bdc698c91d9a4033dd214af867c4a87248a26e
Alex Zippel <alex.zippel@web.de>
2023-05-04 16:32:45 +0200

    correction Syntax

--- components/Marquee.js
+++ components/Marquee.js
@@ -1,23 +1,27 @@
 import styled from "styled-components";
 
 const StyledMarquee = styled.p`
-    font-size: 1.2rem;
-    color: #CCD;
-    position: absolute;
-    white-space: nowrap;
-    line-height: 100%;
-    overflow: hidden;
-    animation: anim 20s linear infinite;
-@keyframes anim{
-    0%{transform: translateX(100%);}
-    100%{transform: translateX(-100%);}
-}
-`
-export default function Marquee(){
-    return(
-            <StyledMarquee>
-                Wenn du das Gefühl hast, dass du am Ende bist, denke daran,
-                dass es nur der Anfang von etwas Neuem sein könnte.
-            </StyledMarquee>
-    )
+  font-size: 1.2rem;
+  color: #DCC;
+  position: absolute;
+  white-space: nowrap;
+  line-height: 100%;
+  overflow: hidden;
+  animation: anim 20s linear infinite;
+  @keyframes anim {
+    0% {
+      transform: translateX(100%);
+    }
+    100% {
+      transform: translateX(-100%);
+    }
+  }
+`;
+export default function Marquee() {
+  return (
+    <StyledMarquee>
+      Wenn du das Gefühl hast, dass du am Ende bist, denke daran, dass es nur
+      der Anfang von etwas Neuem sein könnte.
+    </StyledMarquee>
+  );
 }
--- components/StyledFooter.js
+++ components/StyledFooter.js
@@ -6,17 +6,15 @@ import Marquee from '@/components/Marquee'
 const StyledFooterBar = styled.footer`
     position: fixed;
     bottom: 0;
-    padding: 1rem;
-    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     width: 100%;
     height: 3rem;
     background-color: #334;
     color: #DCC;
     font-size: 0.8rem;
     font-weight: 100;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     box-shadow: 0 0 12px -1px rgba(0, 0, 0, 0.8);
     z-index: 100;
 `
